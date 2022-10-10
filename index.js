f1e12b3a2ff06f6e88d07ab57d60c5a027063d89
thoddusamy <thoddusamyg@gmail.com>
2022-10-10 20:25:58 +0530

    get('/') added

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(cors({ origin: "*" }))
 require('dotenv').config()
 const MainRoutes = require("./Routes/mainRoute")
 
+app.get('/', async (req, res) => {
+    res.send("Server is Working 👍")
+})
+
 app.use("/main", MainRoutes)
 
 app.listen(process.env.PORT || 4999)
